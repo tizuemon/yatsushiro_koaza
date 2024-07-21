ce2d318ac5acb05f5d4359a76ed2841c3c4c3fd0
tizuemon <36941566+tizuemon@users.noreply.github.com>
2024-07-21 12:34:24 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,186 +1,156 @@
-// map定義, 初期の緯度経度指定
-const map = L.map("map").setView([32.5396102, 130.6321314], 14);
+// 地図の初期設定
+const map = L.map("map").setView([32.507, 130.602], 15);
 
-// 地理院地図読み込み
-L.tileLayer("https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png", {
-  attribution:
-    "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>",
+// 地理院地図のレイヤー設定
+const paleMap = L.tileLayer("https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png", {
+  attribution: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>"
 }).addTo(map);
 
-// 地図切り替え
+const standardMap = L.tileLayer("https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png", {
+  attribution: "<a href='http://maps.gsi.go.jp/development/ichiran.html'>地理院タイル</a>"
+});
+
+const reliefMap = L.tileLayer("https://cyberjapandata.gsi.go.jp/xyz/relief/{z}/{x}/{y}.png", {
+  attribution: "<a href='http://maps.gsi.go.jp/development/ichiran.html'>地理院タイル</a>"
+});
+
+const photoMap = L.tileLayer("https://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/{z}/{x}/{y}.jpg", {
+  attribution: "<a href='http://maps.gsi.go.jp/development/ichiran.html'>地理院タイル</a>"
+});
+
+const osmMap = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "© <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
+});
+
+// レイヤーコントロールの追加
 L.control.layers({
-  "淡色地図": L.tileLayer("https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png", {
-    attribution: "<a href='http://maps.gsi.go.jp/development/ichiran.html'>地理院タイル</a>"
-  }).addTo(map),
-  "標準地図": L.tileLayer("https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png", {
-    attribution: "<a href='http://maps.gsi.go.jp/development/ichiran.html'>地理院タイル</a>"
-  }),
-  "色別標高図": L.tileLayer("https://cyberjapandata.gsi.go.jp/xyz/relief/{z}/{x}/{y}.png", {
-    attribution: "<a href='http://maps.gsi.go.jp/development/ichiran.html'>地理院タイル</a>"
-  }),
-  "写真": L.tileLayer("https://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/{z}/{x}/{y}.jpg", {
-    attribution: "<a href='http://maps.gsi.go.jp/development/ichiran.html'>地理院タイル</a>"
-  }),
-  "OpenStreetMap": L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    attribution: "&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
-  })
-
-  // クラス名前設定
-  // className: 'layer_control'
-
+  "淡色地図": paleMap,
+  "標準地図": standardMap,
+  "色別標高図": reliefMap,
+  "写真": photoMap,
+  "OpenStreetMap": osmMap
 }).addTo(map);
 
+// ズームコントロールの位置設定
 map.zoomControl.setPosition("bottomright");
 
+// スケールコントロールの追加
 L.control.scale({
   imperial: false,
   metric: true
 }).addTo(map);
 
-// 前回の緯度経度記憶
-// L.hash(map);
-
-
-// 8色塗分け用
+// 色分け関数
 function getColor(d) {
   return d > 70 ? '#800026' :
-    d > 60 ? '#7FC817' :
-      d > 50 ? '#FF4F90' :
-        d > 40 ? '#FFD453' :
-          d > 30 ? '#FFF2CB' :
-            d > 20 ? '#41D3F3' :
-              d > 10 ? '#795AEE' :
-                '#FFEDA0';
+         d > 60 ? '#7FC817' :
+         d > 50 ? '#FF4F90' :
+         d > 40 ? '#FFD453' :
+         d > 30 ? '#FFF2CB' :
+         d > 20 ? '#41D3F3' :
+         d > 10 ? '#795AEE' :
+                  '#FFEDA0';
 }
 
-// 指定範囲の乱数生成
+// 乱数生成関数
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-// ポリゴンスタイル
-function polygonstyle(feature) {
-
-  return {
-    "color": "rgb(250,10,70)",
-    "weight": 2,
-    "opacity": 0.8,
-    // "fillColor": getColor(getRandomInt(80, 0)),
-    // "fillOpacity": 0.2
-
-    "fillColor": "rgb(255,255,255)",
-    "fillOpacity": 0.2
+// 大字ごとにランダムな色を生成
+const oazaColors = {};
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
   }
-
+  return color;
 }
 
-// geoJSON koaza 読み込み
-$.getJSON("20240602_yatsushiro_koaza.geojson", function (data) {
+// 小字ポリゴンスタイル設定
+function polygonStyle(feature) {
+  const oazaName = feature.properties.大字名;
+  if (!oazaColors[oazaName]) {
+    oazaColors[oazaName] = getRandomColor();
+  }
+  return {
+    color: "rgb(50,50,50)",
+    weight: 1.5,
+    opacity: 0.8,
+    fillColor: oazaColors[oazaName],
+    fillOpacity: 0.3
+  };
+}
 
-  border = L.geoJson(data, {
+// 小字ポリゴンの読み込みと表示
+let koazaMarkers = [];
 
-    // style読み込み
-    style: polygonstyle,
-
+$.getJSON("20240721_yatsushiro_koaza.geojson", function (data) {
+  L.geoJson(data, {
+    style: polygonStyle,
     onEachFeature: function (feature, layer) {
-      let koaza_name = feature.properties.新小字名;
-      let oaza_name = feature.properties.大字名;
-      let polygon_lat = feature.properties.lat;
-      let polygon_lon = feature.properties.lon;
+      const koazaName = feature.properties.新小字名;
+      const oazaName = feature.properties.大字名;
+      const lat = feature.properties.lat;
+      const lon = feature.properties.lon;
 
-      // 小字名表示ラベル
-      var divIcon_koaza = L.divIcon({
-        html: koaza_name,
+      const divIconKoaza = L.divIcon({
+        html: koazaName,
         className: 'divicon_koaza',
         iconSize: [50, 50],
         iconAnchor: [20, 20],
         popupAnchor: [0, 0]
       });
 
-      // 小字名表示
-      L.marker([polygon_lat, polygon_lon], {
-        icon: divIcon_koaza
-      }).addTo(map);
-
-
-      layer.bindPopup('大字 ： ' + oaza_name + '<br>' + '小字 ： ' + koaza_name);
+      const marker = L.marker([lat, lon], { icon: divIconKoaza }).addTo(map);
+      koazaMarkers.push(marker);
+      layer.bindPopup(`大字 ： ${oazaName}<br>小字 ： ${koazaName}`);
     }
-
-    // zIndex: 800
-    
-
   }).addTo(map);
-
-
 });
 
-
-// var myPane = map.createPane("myPaneName");
-// myPane.style.zIndex = 450;
-
-
-// 大字ポリゴンスタイル
-function oaza_polygonstyle(feature) {
-
+// 大字ポリゴンスタイル設定
+function oazaPolygonStyle(feature) {
   return {
-    "color": "rgb(0,0,255)",
-    "weight": 3,
-    // "opacity": 1,
-    // "fillColor": getColor(getRandomInt(80, 0)),
-    "fillOpacity": 0,
-  }
-
+    color: "rgb(0,0,255)",
+    weight: 3,
+    fillOpacity: 0
+  };
 }
 
+// 大字ポリゴンの読み込みと表示
+// $.getJSON("yatsushiro_oaza.geojson", function (data) {
+//   L.geoJson(data, {
+//     style: oazaPolygonStyle,
+//     onEachFeature: function (feature, layer) {
+//       const oazaName = feature.properties.大字名;
+//       const lat = feature.properties.lat;
+//       const lon = feature.properties.lon;
 
-// geoJSON oaza 読み込み
-$.getJSON("20240519_yatsushiro_oaza.geojson", function (data) {
+//       const divIconOaza = L.divIcon({
+//         html: oazaName,
+//         className: 'divicon_oaza',
+//         iconSize: [150, 50],
+//         iconAnchor: [100, 0],
+//         popupAnchor: [0, 0]
+//       });
 
-  oaza_border = L.geoJson(data, {
-
-    // pane: "myPane",
-
-    // style読み込み
-    style: oaza_polygonstyle,
-
-    onEachFeature: function (feature, layer) {
-      // let koaza_name = feature.properties.新小字名;
-      let oaza_oaza_name = feature.properties.大字名;
-      let oaza_polygon_lat = feature.properties.lat;
-      let oaza_polygon_lon = feature.properties.lon;
-
-      // 大字名表示ラベル
-      var divIcon_oaza = L.divIcon({
-        html: oaza_oaza_name,
-        className: 'divicon_oaza',
-        iconSize: [150, 50],
-        iconAnchor: [100, 0],
-        popupAnchor: [0, 0],
-      });
-
-      // 小字名表示
-      L.marker([oaza_polygon_lat, oaza_polygon_lon], {
-        icon: divIcon_oaza
-      }).addTo(map);
-
-      // layer.bindPopup('大字 ： ' + oaza_oaza_name + '<br>' + '小字 ： ' + koaza_name);
-    }
-
-    // zIndex: 9000
-
-  }).addTo(map);
-
-});
-
-
-// スクロール判定
-
-// window.addEventListener('click', function() {
-//   console.log('クリックされました');
-//   var response = map.getZoom();
-//   if(response >=14){
-//     console.log('ズームレベル：14以上');
-//   }
+//       L.marker([lat, lon], { icon: divIconOaza }).addTo(map);
+//     }
+//   }).addTo(map);
 // });
+
+// ズームレベルに応じたラベルの表示・非表示
+map.on('zoomend', function() {
+  const zoomLevel = map.getZoom();
+  koazaMarkers.forEach(marker => {
+    if (zoomLevel < 15) {
+      map.removeLayer(marker);
+    } else {
+      map.addLayer(marker);
+    }
+  });
+});
